b95fed8f815c9815c145911b6b848a2e7a98d8b6
dbroglie <deb65@pitt.edu>
2014-09-22 22:59:37 -0400

    index html and dropdown js

---
+++ funcs.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+
+function createTeamDropDown(league) {
+  var x = 0, options, option_str, name;
+
+  if (league === 'MLB') {
+    teams = JSON.parse(mlbTeams);
+	console.out(teams);
+  } else if (league === 'NBA') {
+    teams = JSON.parse(nbaTeams);
+  } else if (league === 'NFL') {
+    teams = JSON.parse(nflTeams);
+  } else if (league === 'NHL') {
+    teams = JSON.parse(nhlTeams);
+  }
+
+  while (teams.teams[x] != NULL) {
+    name = teams.teams[x].location + ' ' + teams.teams[x].mascot;
+    option_str = '<option value="' + name +'">' + name + '</option>';
+    options = options + option_str;
+	x++;
+  }
+
+  return options;
+}
