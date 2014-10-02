11bfa1f1cbf60a37a3f5c84b357323fdd4afda48
Drew Winfield <daw106@pitt.edu>
2014-10-02 15:09:09 -0400

    Syncing fool project incl. yaml
    Yaml, libraries, python scripts, js, html

---
+++ scripts/funcs.js
@@ -0,0 +1,550 @@
+function populateTeams(league) {
+	var x = 0, options, option_str, name;
+	options = '<option disabled="disabled" selected="selected"></option>';
+
+	if (league === 'mlb') {
+		teams = mlbTeams;
+		//teams = JSON.parse(mlbTeams);
+	} else if (league === 'nba') {
+		teams = nbaTeams;
+		//teams = JSON.parse(nbaTeams);
+	} else if (league === 'nfl') {
+		teams = nflTeams;
+		//teams = JSON.parse(nflTeams);
+	} else if (league === 'nhl') {
+		teams = nhlTeams;
+		//teams = JSON.parse(nhlTeams);
+	}
+	
+	while (teams[x] != null) {
+		name = teams[x].location + ' ' + teams[x].mascot;
+		option_str = '<option value="' + name +'">' + name + '</option>';
+		console.log(option_str);
+		options = options + option_str;
+		x++;
+	}
+
+	return options;
+}
+
+function createTeamsDropDown(teams) {
+	var league = document.getElementById('leagues').value;
+	var teamSelect = document.getElementById('teams');
+	// display team dropdown
+	teamSelect.style.display = 'block';
+	teamSelect.innerHTML = populateTeams(league);
+	// display submit button
+	var submit = document.getElementById('submit-search');
+	submit.style.display = 'block';
+}
+
+function chooseLeagueAndTeam() {
+	var leagues = document.getElementById('leagues');
+	var chosenLeague = leagues.options[leagues.selectedIndex].text;
+	var league = document.getElementById('league');
+	league.value = chosenLeague;
+	
+	var teams = document.getElementById('teams');
+	var chosenTeam = teams.options[teams.selectedIndex].text;
+	var team = document.getElementById('team');
+	team.value = chosenTeam;
+ 
+  document.searchForm.submit();
+	
+}
+
+var mlbTeams = [
+            {
+                location: "Arizona",
+                mascot: "Diamondbacks"
+            },
+            {
+                location: "Atlanta",
+                mascot: "Braves"
+            },
+            {
+                location: "Baltimore",
+                mascot: "Orioles"
+            },
+            {
+                location: "Boston",
+                mascot: "Red Sox"
+            },
+            {
+                location: "Chicago",
+                mascot: "White Sox"
+            },
+            {
+                location: "Chicago",
+                mascot: "Cubs"
+            },
+            {
+                location: "Cincinnati",
+                mascot: "Reds"
+            },
+            {
+                location: "Cleveland",
+                mascot: "Indians"
+            },
+            {
+                location: "Colorado",
+                mascot: "Rockies"
+            },
+            {
+                location: "Detroit",
+                mascot: "Tigers"
+            },
+            {
+                location: "Florida",
+                mascot: "Marlins"
+            },
+            {
+                location: "Houston",
+                mascot: "Astros"
+            },
+            {
+                location: "Kansas City",
+                mascot: "Royals"
+            },
+            {
+                location: "Los Angeles",
+                mascot: "Angels"
+            },
+            {
+                location: "Los Angeles",
+                mascot: "Dodgers"
+            },
+            {
+                location: "Milwaukee",
+                mascot: "Brewers"
+            },
+            {
+                location: "Minnesota",
+                mascot: "Twins"
+            },
+            {
+                location: "New York",
+                mascot: "Mets"
+            },
+            {
+                location: "New York",
+                mascot: "Yankees"
+            },
+            {
+                location: "Oakland",
+                mascot: "Athletics"
+            },
+            {
+                location: "Philadelphia",
+                mascot: "Phillies"
+            },
+            {
+                location: "Pittsburgh",
+                mascot: "Pirates"
+            },
+            {
+                location: "San Diego",
+                mascot: "Padres"
+            },
+            {
+                location: "San Francisco",
+                mascot: "Giants"
+            },
+            {
+                location: "Seattle",
+                mascot: "Mariners"
+            },
+            {
+                location: "St. Louis",
+                mascot: "Cardinals"
+            },
+            {
+                location: "Tampa Bay",
+                mascot: "Rays"
+            },
+            {
+                location: "Texas",
+                mascot: "Rangers"
+            },
+            {
+                location: "Toronto",
+                mascot: "Blue Jays"
+            },
+            {
+                location: "Washington",
+                mascot: "Nationals"
+            }
+        ];
+
+var nbaTeams = [
+            {
+                location: "Atlanta",
+                mascot: "Hawks"
+            },
+			{
+                location: "Boston",
+                mascot: "Celtics"
+            },
+			{
+                location: "Charlotte",
+                mascot: "Bobcats"
+            },
+			{
+                location: "Chicago",
+                mascot: "Bulls"
+            },
+			{
+                location: "Cleveland",
+                mascot: "Cavaliers"
+            },
+			{
+                location: "Dallas",
+                mascot: "Mavericks"
+            },
+			{
+                location: "Denver",
+                mascot: "Nuggets"
+            },
+			{
+                location: "Detroit",
+                mascot: "Pistons"
+            },
+			{
+                location: "Golden State",
+                mascot: "Warriors"
+            },
+			{
+                location: "Houston",
+                mascot: "Rockets"
+            },
+			{
+                location: "Indiana",
+                mascot: "Pacers"
+            },
+			{
+                location: "LA",
+                mascot: "Clippers"
+            },
+			{
+                location: "LA",
+                mascot: "Lakers"
+            },
+			{
+                location: "Memphis",
+                mascot: "Grizzlies"
+            },
+			{
+                location: "Miami",
+                mascot: "Heat"
+            },
+			{
+                location: "Milwaukee",
+                mascot: "Timberwolves"
+            },
+			{
+                location: "New Jersey",
+                mascot: "Nets"
+            },
+			{
+                location: "New Orleans",
+                mascot: "Hornets"
+            },
+			{
+                location: "New York",
+                mascot: "Knicks"
+            },
+			{
+                location: "Oklahoma City",
+                mascot: "Thunder"
+            },
+			{
+                location: "Orlando",
+                mascot: "Magic"
+            },
+			{
+                location: "Philadelphia",
+                mascot: "Sixers"
+            },
+			{
+                location: "Phoenix",
+                mascot: "Suns"
+            },
+			{
+                location: "Portland",
+                mascot: "Trail Blazers"
+            },
+			{
+                location: "Sacramento",
+                mascot: "Kings"
+            },
+			{
+                location: "San Antonio",
+                mascot: "Spurs"
+            },
+			{
+                location: "Toronto",
+                mascot: "Raptors"
+            },
+			{
+                location: "Utah",
+                mascot: "Jazz"
+            },
+			{
+                location: "Washington",
+                mascot: "Wizards"
+            }
+		];
+
+var nflTeams = [
+			{
+                location: "Arizona",
+                mascot: "Cardinals"
+            },
+			{
+                location: "Atlanta",
+                mascot: "Falcons"
+            },
+			{
+                location: "Baltimore",
+                mascot: "Ravens"
+            },
+			{
+                location: "Buffalo",
+                mascot: "Bills"
+            },
+			{
+                location: "Carolina",
+                mascot: "Panthers"
+            },
+			{
+                location: "Chicago",
+                mascot: "Bears"
+            },
+			{
+                location: "Cincinnati",
+                mascot: "Bengals"
+            },
+			{
+                location: "Cleveland",
+                mascot: "Browns"
+            },
+			{
+                location: "Dallas",
+                mascot: "Cowboys"
+            },
+			{
+                location: "Denver",
+                mascot: "Broncos"
+            },
+			{
+                location: "Detroit",
+                mascot: "Lions"
+            },
+			{
+                location: "Green Bay",
+                mascot: "Packers"
+            },
+			{
+                location: "Houston",
+                mascot: "Texans"
+            },
+			{
+                location: "Indianapolis",
+                mascot: "Colts"
+            },
+			{
+                location: "Jacksonville",
+                mascot: "Jaguars"
+            },
+			{
+                location: "Kansas City",
+                mascot: "Chiefs"
+            },
+			{
+                location: "Miami",
+                mascot: "Dolphins"
+            },
+			{
+                location: "Minnesota",
+                mascot: "Vikings"
+            },
+			{
+                location: "New England",
+                mascot: "Patriots"
+            },
+			{
+                location: "New Orleans",
+                mascot: "Saints"
+            },
+			{
+                location: "New York",
+                mascot: "Giants"
+            },
+			{
+                location: "New York",
+                mascot: "Jets"
+            },
+			{
+                location: "Oakland",
+                mascot: "Raiders"
+            },
+			{
+                location: "Philadelphia",
+                mascot: "Eagles"
+            },
+			{
+                location: "Pittsburgh",
+                mascot: "Steelers"
+            },
+			{
+                location: "Saint Louis",
+                mascot: "Rams"
+            },
+			{
+                location: "San Diego",
+                mascot: "Chargers"
+            },
+			{
+                location: "San Francisco",
+                mascot: "49ers"
+            },
+			{
+                location: "Seattle",
+                mascot: "Seahawks"
+            },
+			{
+                location: "Tampa Bay",
+                mascot: "Buccaneers"
+            },
+			{
+                location: "Tennessee",
+                mascot: "Titans"
+            },
+			{
+                location: "Washington",
+                mascot: "Redskins"
+            }
+		];
+		
+var nhlTeams = [
+			{
+                location: "Anaheim",
+                mascot: "Ducks"
+            },
+			{
+                location: "Atlanta",
+                mascot: "Thrashers"
+            },
+			{
+                location: "Boston",
+                mascot: "Bruins"
+            },
+			{
+                location: "Buffalo",
+                mascot: "Sabres"
+            },
+			{
+                location: "Calgary",
+                mascot: "Flames"
+            },
+			{
+                location: "Carolina",
+                mascot: "Hurricanes"
+            },
+			{
+                location: "Chicago",
+                mascot: "Blackhawks"
+            },
+			{
+                location: "Colorado",
+                mascot: "Avalanche"
+            },
+			{
+                location: "Columbus",
+                mascot: "Blue Jackets"
+            },
+			{
+                location: "Dallas",
+                mascot: "Stars"
+            },
+			{
+                location: "Detroit",
+                mascot: "Red Wings"
+            },
+			{
+                location: "Edmonton",
+                mascot: "Oilers"
+            },
+			{
+                location: "Florida",
+                mascot: "Panthers"
+            },
+			{
+                location: "Los Angeles",
+                mascot: "Kings"
+            },
+			{
+                location: "Minnesota",
+                mascot: "Wild"
+            },
+			{
+                location: "Montreal",
+                mascot: "Canadiens"
+            },
+			{
+                location: "Nashville",
+                mascot: "Predators"
+            },
+			{
+                location: "New Jersey",
+                mascot: "Devils"
+            },
+			{
+                location: "New York",
+                mascot: "Islanders"
+            },
+			{
+                location: "New York",
+                mascot: "Rangers"
+            },
+			{
+                location: "Ottawa",
+                mascot: "Senators"
+            },
+			{
+                location: "Philadelphia",
+                mascot: "Flyers"
+            },
+			{
+                location: "Phoenix",
+                mascot: "Coyotes"
+            },
+			{
+                location: "Pittsburgh",
+                mascot: "Penguins"
+            },
+			{
+                location: "Saint Louis",
+                mascot: "Blues"
+            },
+			{
+                location: "San Jose",
+                mascot: "Sharks"
+            },
+			{
+                location: "Tampa Bay",
+                mascot: "Lightning"
+            },
+			{
+                location: "Toronto",
+                mascot: "Maple Leafs"
+            },
+			{
+                location: "Vancouver",
+                mascot: "Canucks"
+            },
+			{
+                location: "Washington",
+                mascot: "Capitals"
+            }
+		];
---
+++ server.py
@@ -0,0 +1,278 @@
+import webapp2
+from google.appengine.ext import db
+from httplib import *
+from urllib import urlopen
+from HTMLParser import HTMLParser
+import re
+from threading import Thread
+import Queue
+import bs4
+
+q = Queue.Queue()
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-type'] = 'text/html'
+
+        filename = 'index.html'
+        
+        f = open(filename, 'r')
+        myresponse = f.read()
+
+        self.response.out.write(myresponse)
+
+class SearchQuery(webapp2.RequestHandler):
+
+    def post(self):
+      self.search()
+      
+    def find_site(self, team, league, main_site):
+      
+      html = self.get_html(main_site + '/' + league + '/teams')
+      # look for whole team name, just location, and just mascot
+      # return stuff around the locations
+      # make sure we're dealing with strings
+      team = str(team)
+      html = str(html)
+      
+      # everything lower case for better matching
+      team = str.lower(team)
+      html = str.lower(html)
+      
+      # split the team name into two parts
+      location = ''
+      mascot = ''
+      
+      if len(team.split(' ')) == 2:
+        location = team[:str.find(team, ' ')]
+        mascot = team[str.find(team, ' '):].strip()
+      else:
+        location = team[:str.find(team, ' ')]
+        location += ' ' + team[str.find(team, ' '):str.rfind(team, ' ')].strip()
+        mascot = team[str.rfind(team, ' '):].strip()
+
+      team_parts = team.split(' ')
+
+      parser = League_parser(location, mascot)
+
+      parser.feed(html)
+      links = parser.links
+      
+      the_link = self.choose_link(links, location, league)
+      
+      # kind of terrible fix for SI problem
+      if team_parts[-2] not in the_link and 'teams' not in the_link:
+        the_link = '/nfl/team/'
+        for part in team_parts:
+          the_link += part
+          if part is not team_parts[-1]:
+            the_link += '-'
+            
+      # build out relative links
+      # complete relative links
+      if '.com' not in the_link:
+        the_link = main_site + the_link
+      q.put(the_link)
+
+    # ok, how to choose which one
+    # I think picking the shortest will probably work almost always
+    # do this for now (a keyword check is probably a good idea, too)
+    # measure shortest from the .com, to prevent the domain from being included
+    def choose_link(self, links, location, league):
+      the_link = ''
+      i = 0
+      links = sorted(links, key=len)
+      try:
+        the_link = links[0]
+      except IndexError:
+        pass
+      
+      return the_link
+
+
+    def search(self):
+      team = self.request.get("team")
+      path = 'text_files/sports_sites/'
+      filename = 'sports_sites.txt'
+      sites = []
+      with open(path + filename) as f:
+            sites = f.readlines()
+      
+      for i, site in enumerate(sites):
+        sites[i] = site.strip()
+      
+      # figure out which league to look in
+      league = ''
+      path = 'text_files/teams/'
+      match = False
+      files = os.listdir(path)
+      for t in files:
+        list_name = t[:str.find(t, '-')]
+        with open(path + t) as f:
+          teams = f.readlines()
+        for option in teams:
+          if team == option.strip():
+            match = True
+            break
+        if match:
+          league = list_name
+          break
+        
+      team_sites = []
+      threads = []
+      # go to sites and find team page
+      for site in sites:
+        threads.append(Thread(target=self.find_site, args = (team, league, site)))
+
+      for thread in threads:
+        thread.start()
+        
+      for thread in threads:
+        thread.join()
+            
+      while q.empty() is not True:
+        team_sites.append(q.get())
+
+      threads = []
+      # go and get the team pages
+      for site in team_sites:
+        #self.get_team_site(site)
+        threads.append(Thread(target=self.get_team_site, args = (site,)))
+               
+      for thread in threads:
+        thread.start()
+      
+      for thread in threads:
+        thread.join()      
+     
+      # parse team sites for articles
+      # this kinda sucks too
+      # will do better job parsing in future
+      parse_key = {}
+      for site in sites:
+        if 'espn' in site:
+          parse_key[site] = 'mod-content'
+        elif 'si' in site:
+          parse_key[site] = 'river-body'
+        elif 'bleacher' in site:
+          parse_key[site] = 'team-stream'
+        elif 'yahoo' in site:
+          parse_key[site] = 'bd'
+        elif 'fox' in site:
+          parse_key[site] = 'buzz-container'
+
+      articles = {}
+      team_html = []
+      while q.empty() is not True:
+        team_html.append(q.get())
+        
+      for site in sites:
+        html = ''
+        for url, data in team_html:
+          if site in url:
+            html = data
+            break
+        articles[site] = self.get_articles(parse_key, site, html)
+
+      # output
+      #self.response.out.write("<html><body>")
+      
+
+
+      self.response.out.write(str(team_html[1][1]))
+
+
+
+      #self.response.out.write("</body></html>")
+    
+    def get_articles(self, parse_key, site, html):
+      parser = Team_parser(parse_key, site)
+      parser.feed(html)
+      site_articles = parser.links
+      return site_articles
+
+    def get_team_site(self, site):
+      data = self.get_html(site)
+      q.put((site, data))
+  
+    def get_html(self, site):
+      try:
+        f = urlopen(site)
+        data = f.read()
+      except HTTPException:
+        data = ''
+      return data
+      
+class League_parser(HTMLParser):
+  def __init__(self, location, mascot):
+    HTMLParser.__init__(self)
+    self.location = location
+    self.mascot = mascot
+    self.last_link = ''
+    self.links = []    
+  
+  # essentially here we're looking for the location of the team in the web page
+  # then going back to the last <a> tag (last_link) and adding it to the list
+  # it works well, since most links look like: <a ...> Team </a>
+  # of course, not all... It doesn't work for SI
+  def handle_starttag(self, tag, attrs):
+    if tag == 'a':
+      for name, value in attrs:
+        if name == 'href':
+          self.last_link = value
+             
+  def handle_data(self, data):
+    if self.location in data:
+      self.links.append(self.last_link)
+    
+class Team_parser(HTMLParser):
+  def __init__(self, parse_key, site):
+    HTMLParser.__init__(self)
+    self.parse_keys = parse_key
+    self.the_site = site
+    self.in_content = False
+    self.get_data = False
+    self.links = [] 
+    self.titles = []
+    self.div_levels = 0
+    
+  # cheating a little bit, we know which element class to look in
+  # using the parse_key dict, so get all links (and title values or
+  # data value, if no title in <a> tag)
+  def handle_starttag(self, tag, attrs):
+    if tag == 'div':
+      if self.in_content:
+        self.div_levels += 1
+      for name, value in attrs:
+        if name == 'id' or name == 'class':
+          if self.parse_keys[self.the_site] in value:
+            self.in_content = True
+            self.div_levels += 1
+            
+    if tag == 'a':
+      self.get_data = True
+      if self.in_content:
+        for name, value in attrs:
+          if name == 'href':
+            self.links.append(value)
+          if name == 'title':
+            self.titles.append(value)
+            self.get_data = False
+  
+  def handle_endtag(self, tag):
+    if tag == 'div':
+      self.div_levels -= 1
+      if self.div_levels == 0:
+        self.in_content = False
+
+  def handle_data(self, data):
+    if self.get_data:
+      self.titles.append(data)
+      self.get_data = False
+  
+  
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/search', SearchQuery)
+], debug=True)
