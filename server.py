c8d24cce56860713b8d9f9d15bd40c4282a3bcf9
Drew Winfield <daw106@pitt.edu>
2014-10-29 23:07:44 -0400

    So much things
    Article grab from db.  Article cron fetches working

--- server.py
+++ server.py
@@ -40,7 +40,7 @@ def article_key(article_name=DEFAULT_ARTICLE_NAME):
     return ndb.Key('Article', article_name)
 
 class Article(ndb.Model):
-    metadata = ndb.StringProperty()
+    metadata = ndb.StringProperty(indexed=False)
     team = ndb.StringProperty()
     date = ndb.DateTimeProperty(auto_now_add=True)
     
@@ -50,6 +50,18 @@ def user_key(user_name=DEFAULT_USER_NAME):
 	"""creates a Datastore key for a User entity with user_name"""
 	return ndb.Key('User', user_name)
 	
+class UserTeams(ndb.Model):
+	user = ndb.UserProperty()
+	teams = ndb.LocalStructuredProperty(Team, repeated=True)
+
+class ViewedArticles(ndb.Model):
+	user = ndb.UserProperty()
+	articles = ndb.StructuredProperty(Article, repeated=True)
+
+class Favorites(ndb.Model):
+	user = ndb.UserProperty()
+	articles = ndb.StructuredProperty(Article, repeated=True)
+
 def search(league):
 
     path = 'text_files/sports_sites/'
@@ -217,18 +229,73 @@ def get_team_links(sites, team, league):
             links.append(entry + links_dict[entry])
         
     return links
-	
-class UserTeams(ndb.Model):
-	user = ndb.UserProperty()
-	teams = ndb.LocalStructuredProperty(Team, repeated=True)
 
-class ViewedArticles(ndb.Model):
-	user = ndb.UserProperty()
-	articles = ndb.StructuredProperty(Article, repeated=True)
+def get_articles(league):
 
-class Favorites(ndb.Model):
-	user = ndb.UserProperty()
-	articles = ndb.StructuredProperty(Article, repeated=True)
+    """
+    Search Table
+    ESPN : class="result"
+    SI :   class="list-item"
+    BR : "article" in tag["class"]
+
+    """
+    article_classes = {'espn': 'result', 'si': 'list-item', 'bleacherreport': 'block-list_item'}
+    articles = []
+    team_query = Team.query((Team.league == league), ancestor=team_key(DEFAULT_TEAM_NAME))
+    team_entity = team_query.fetch(None)
+
+    for team in team_entity:
+        links = []
+        for site in team.sites:
+            links.append(site.url)
+        
+        for link in links:
+            per_site_counter = 0
+            data = ''
+            try:
+                f = urlopen(link)
+                data = f.read()
+            except HTTPException:
+                data = ''
+
+            # use bs to find some stuff
+            soup = BeautifulSoup(data)
+    
+            domain = link[link.find('.') + 1:link.rfind('.com')]
+            article_class = ''
+            try:
+                article_class = article_classes[domain]
+            except KeyError:
+                continue
+      
+            article_class = unicode(article_class)
+            #articles.append(link)
+            #articles.append(article_key)
+        
+            # get the tags corresponding to the keys
+            for tag in soup.find_all(True):
+                try:
+                    class_val = tag['class']
+                    #articles.append((class_val, len(class_val)))
+                    if len(class_val) == 1:
+                        if article_class in class_val:
+                            # add article to data store
+                            article = Article(parent=article_key(DEFAULT_ARTICLE_NAME))
+                            article.metadata = str(tag)
+                            article.team = team.name
+                            article.put()
+                    elif 'bleacherreport' in link:
+                        if article_class in class_val:
+                            # add article to data store
+                            article = Article(parent=article_key(DEFAULT_ARTICLE_NAME))
+                            article.metadata = str(tag)
+                            article.team = team.name
+                            article.put()
+                except (KeyError, UnicodeDecodeError):
+                    pass
+        
+    articles_query = Article.query(ancestor=article_key(DEFAULT_ARTICLE_NAME))
+    article_list = articles_query.fetch(None)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -282,55 +349,17 @@ class SearchQuery(webapp2.RequestHandler):
     def search(self):
        
         team = self.request.get("team")
-        path = 'text_files/sports_sites/'
-        filename = 'sports_sites.txt'
-        sites = []
-        with open(path + filename) as f:
-            sites = f.readlines()
+        league = self.request.get("league")
+        league = league.lower()        
       
-        for i, site in enumerate(sites):
-            sites[i] = site.strip()
+        articles_query = Article.query((Article.team == team), ancestor=article_key(DEFAULT_ARTICLE_NAME))
+        article_list = articles_query.fetch(20)
       
-        # figure out which league to look in
-        league = ''
-        path = 'text_files/teams/'
-        match = False
-        files = os.listdir(path)
-        for t in files:
-            if t[0] == '.':
-                break
-            list_name = t[:str.find(t, '-')]
-            with open(path + t) as f:
-                teams = f.readlines()
-            for option in teams:
-                if team == option.strip():
-                    match = True
-                    break
-            if match:
-                league = list_name
-                break
-    
-        links_dict = self.get_team_links(sites, team, league)
-      
-        # ok we have the team pages 
-        # should probably store them and then just retrieve them from datastore
-        # for future calls
-        links = []
-        for entry in links_dict:
-            # link might already be absolute, don't add domain if that's the case
-            if '.com' in links_dict[entry]:
-                link_parts = links_dict[entry].split('.com')
-                links.append(entry + link_parts[1])
-            else:
-                links.append(entry + links_dict[entry])
-          
-        # get articles
-        articles = self.get_articles(links)
-      
-      
+        """
         template_values = {
             'articles' : articles
         }
+        """
       
         # output
         filename = 'search_header.html'
@@ -347,167 +376,11 @@ class SearchQuery(webapp2.RequestHandler):
           
         self.response.out.write(header)
         
-        for article in articles:
-            self.response.out.write(article)
+        for article in article_list:
+            self.response.out.write(article.metadata)
             self.response.out.write('<br>')
             
         self.response.out.write(footer)
-        
-      
-    def get_team_links(self, sites, team, league):
-    
-        team = str(team)
-        # split team into location and mascot
-        if len(team.split(' ')) == 2:
-            location = team[:str.find(team, ' ')]
-            mascot = team[str.find(team, ' '):].strip()
-        else:
-            location = team[:str.find(team, ' ')]
-            location += ' ' + team[str.find(team, ' '):str.rfind(team, ' ')].strip()
-            mascot = team[str.rfind(team, ' '):].strip()
-        #"""
-      
-        # lower case mascots and locations
-        location = location.lower()
-        mascot = mascot.lower()
-        links_dict = {}
-        # use urllib to get html
-        for site in sites:
-            url = site + '/' + league + '/teams'
-            data = ''
-            try:
-                f = urlopen(url)
-                #site = f.geturl()
-                data = f.read()
-            except HTTPException:
-                data = ''
-    
-            # use bs to find some stuff
-            soup = BeautifulSoup(data)
-      
-            all_links = soup.find_all('a')
-            text = []
-            links_team_filter = []
-            the_link = ''
-            for link in all_links:
-                try:
-                    if 'team' in str(link['href']):
-                        links_team_filter.append(link)
-                except (KeyError, UnicodeEncodeError):
-                    pass
-        
-            # if the mascot is in the link text, then we want it
-            for link in links_team_filter:
-                try:
-                    if mascot in str(link.text).lower():
-                        the_link = str(link['href'])
-                except (KeyError, UnicodeEncodeError):
-                    pass
-
-            # if not found in previous step, look for location in link text
-            if the_link == '':
-                for link in links_team_filter:
-                    try:
-                        if location in str(link.text).lower():
-                            the_link = str(link['href'])
-                    except (KeyError, UnicodeEncodeError):
-                        pass
-                
-            # if the mascot is in the link text, then we want it
-            if the_link == '':
-                for link in all_links:
-                    try:
-                        if mascot in str(link.text).lower():
-                            the_link = str(link['href'])
-                    except (KeyError, UnicodeEncodeError):
-                         pass
-
-            # if not found in previous step, look for location in link text
-            if the_link == '':
-                for link in all_links:
-                    try:
-                        if location in str(link.text).lower():
-                            the_link = str(link['href'])
-                    except (KeyError, UnicodeEncodeError):
-                        pass
-                
-            # still none found? Well... look at the href values
-            # for mascot
-            if the_link == '':
-                for link in all_links:
-                    try:
-                        if mascot in str(link['href']).lower():
-                            the_link = str(link['href'])
-                    except (KeyError, UnicodeEncodeError):
-                        pass
-            # and then for location, with dashes replacing spaces
-            if the_link == '':
-                for link in all_links:
-                    try:
-                        if location.replace(' ', '-') in str(link['href']).lower():
-                            the_link = str(link['href'])
-                    except (KeyError, UnicodeEncodeError):
-                        pass
-                  
-            links_dict[site] = the_link
-            #"""
-        return links_dict
-    
-    def get_articles(self, links):
-    
-        """
-        Search Table
-        ESPN : class="result"
-        SI :   class="list-item"
-        BR : "article" in tag["class"]
-
-        """
-        article_keys = {'espn': 'result', 'si': 'list-item', 'bleacherreport': 'block-list_item'}
-        articles = []
-        for link in links:
-            per_site_counter = 0
-            data = ''
-            try:
-                f = urlopen(link)
-                data = f.read()
-            except HTTPException:
-                data = ''
-    
-            # use bs to find some stuff
-            soup = BeautifulSoup(data)
-        
-            domain = link[link.find('.') + 1:link.rfind('.com')]
-            article_key = ''
-            try:
-                article_key = article_keys[domain]
-            except KeyError:
-                continue
-          
-            article_key = unicode(article_key)
-            #articles.append(link)
-            #articles.append(article_key)
-            
-            # get the tags corresponding to the keys
-            if per_site_counter > 10:
-                break
-            for tag in soup.find_all(True):
-                try:
-                    class_val = tag['class']
-                    #articles.append((class_val, len(class_val)))
-                    if len(class_val) == 1:
-                        if article_key in class_val:
-                            articles.append(tag)
-                            per_site_counter += 1
-                    elif 'bleacherreport' in link:
-                        if article_key in class_val:
-                            articles.append(tag)
-                            per_site_counter += 1
-                except (KeyError, UnicodeDecodeError):
-                    pass
-            
-            #articles += soup.find_all()
-            
-        return articles
 
 class Feed(webapp2.RequestHandler):
     def get(self):
@@ -587,106 +460,98 @@ class ChooseTeams(webapp2.RequestHandler):
         
         self.redirect('/feed')
         
-# CRON JOBS
+#--------------------------- CRON JOBS ----------------------------#
 class GetNFLSites(webapp2.RequestHandler):
     def get(self):
         info = search('nfl')
-        
+        """
         self.response.out.write('<html><body>')
         self.response.out.write(str(info))
         self.response.out.write('</body></html>')
-
+        """
     def post(self):
         info = search('nfl')
         
 class GetNBASites(webapp2.RequestHandler):
     def get(self):
         info = search('nba')
+        """
         self.response.out.write('<html><body>')
         self.response.out.write(str(info))
         self.response.out.write('</body></html>')
-
+        """
     def post(self):
         info = search('nba')
         
 class GetNHLSites(webapp2.RequestHandler):
     def get(self):
         info = search('nhl')
+        """
         self.response.out.write('<html><body>')
         self.response.out.write(str(info))
         self.response.out.write('</body></html>')
+        """
     def post(self):
         info = search('nhl')
         
 class GetMLBSites(webapp2.RequestHandler):
     def get(self):
         info = search('mlb')
+        """
         self.response.out.write('<html><body>')
         self.response.out.write(str(info))
         self.response.out.write('</body></html>')
+        """
     def post(self):
         info = search('mlb')
                 
 class GetNFLArticles(webapp2.RequestHandler):
 
     def get(self):
-        self.get(articles)
-        
-    def get_articles(self, links):
-    
+        get_articles('nfl')
         """
-        Search Table
-        ESPN : class="result"
-        SI :   class="list-item"
-        BR : "article" in tag["class"]
+        self.response.out.write('<html><body>')
+        self.response.out.write(str(article_list))
+        self.response.out.write('</body></html>')
+        """
+        
+    def post(self):
+        get_articles('nfl')      
+class GetNBAArticles(webapp2.RequestHandler):
 
+    def get(self):
+        get_articles('nba')
         """
-        article_keys = {'espn': 'result', 'si': 'list-item', 'bleacherreport': 'block-list_item'}
-        articles = []
-        for link in links:
-            per_site_counter = 0
-            data = ''
-            try:
-                f = urlopen(link)
-                data = f.read()
-            except HTTPException:
-                data = ''
-    
-            # use bs to find some stuff
-            soup = BeautifulSoup(data)
+        self.response.out.write('<html><body>')
+        self.response.out.write(str(article_list))
+        self.response.out.write('</body></html>')
+        """
+    def post(self):
+        get_articles('nba')
+                
+class GetNHLArticles(webapp2.RequestHandler):
+
+    def get(self):
+        get_articles('nhl')
+        """
+        self.response.out.write('<html><body>')
+        self.response.out.write(str(article_list))
+        self.response.out.write('</body></html>')
+        """
         
-            domain = link[link.find('.') + 1:link.rfind('.com')]
-            article_key = ''
-            try:
-                article_key = article_keys[domain]
-            except KeyError:
-                continue
-          
-            article_key = unicode(article_key)
-            #articles.append(link)
-            #articles.append(article_key)
-            
-            # get the tags corresponding to the keys
-            if per_site_counter > 10:
-                break
-            for tag in soup.find_all(True):
-                try:
-                    class_val = tag['class']
-                    #articles.append((class_val, len(class_val)))
-                    if len(class_val) == 1:
-                        if article_key in class_val:
-                            articles.append(tag)
-                            per_site_counter += 1
-                    elif 'bleacherreport' in link:
-                        if article_key in class_val:
-                            articles.append(tag)
-                            per_site_counter += 1
-                except (KeyError, UnicodeDecodeError):
-                    pass
-            
-            #articles += soup.find_all()
-            
-        return articles
+    def post(self):
+        get_articles('nhl')      
+class GetMLBArticles(webapp2.RequestHandler):
+
+    def get(self):
+        get_articles('mlb')
+        #"""
+        self.response.out.write('<html><body>')
+        self.response.out.write(str(article_list))
+        self.response.out.write('</body></html>')
+        #"""
+    def post(self):
+        get_articles('mlb')
 
 class GetTeams(webapp2.RequestHandler):
     def post(self):
@@ -715,7 +580,7 @@ app = webapp2.WSGIApplication([
     ('/getNHLSites', GetNHLSites),
     ('/getMLBSites', GetMLBSites),
     ('/getNFLArticles', GetNFLArticles),
-    ('/getNBAArticles', GetNFLArticles),
-    ('/getNHLArticles', GetNFLArticles),
-    ('/getMLBArticles', GetNFLArticles)
+    ('/getNBAArticles', GetNBAArticles),
+    ('/getNHLArticles', GetNHLArticles),
+    ('/getMLBArticles', GetMLBArticles)
 ], debug=True)
